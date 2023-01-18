fc1967fd338ca52246fbe7bd07b8d02207417a3d
Helen Shin <helenhyshin@gmail.com>
2023-01-18 13:49:48 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#link-form").classList.remove("hide");
+
+  const input = document.querySelector("#message-input");
+
+  // btoa encrypts the message that the user enters in the input above
+  const encrypted = btoa(input.value);
+  // sets the input value displayed as the encrypted value
+  const linkInput = document.querySelector("#link-input");
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
+});
